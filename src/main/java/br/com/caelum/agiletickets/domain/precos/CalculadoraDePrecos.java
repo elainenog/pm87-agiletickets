ed0f9692cde0ae882710579acc820fd12496e134
elaine <elainenog@yahoo.com.br>
2014-08-21 22:45:32 -0300

    arrumando merge

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,7 +9,6 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-<<<<<<< HEAD
 		double aliquota = 0.0;
 		double lotacao = 0.0;
 		boolean temAdicionalTempo = false;
@@ -61,57 +60,4 @@ public class CalculadoraDePrecos {
 		return sessao.getTotalIngressos() - sessao.getIngressosReservados();
 	}
 
-=======
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			//quando estiver acabando os ingressos... 
-			
-			if(verificarQuantidadeIngressos(sessao, 0.05)) { 
-				preco = calculaPrecoSessao(sessao, 0.10);
-			} else {
-				preco = sessao.getPreco();
-			}
-			
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			
-			if(verificarQuantidadeIngressos(sessao, 0.50)) { 
-				preco = calculaPrecoSessao(sessao, 0.20);
-			} else {
-				preco = sessao.getPreco();
-			}
-			
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(multiplicaPrecoSessao(sessao,0.10));
-			}
-			
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			
-			if(verificarQuantidadeIngressos(sessao, 0.50)) { 
-				preco = calculaPrecoSessao(sessao, 0.20);
-			} else {
-				preco = sessao.getPreco();
-			}
-
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(multiplicaPrecoSessao(sessao,0.10));
-			}
-			
-		}  else {
-			//nao aplica aumento para teatro (quem vai é pobretão)
-			preco = sessao.getPreco();
-		} 
-		return preco.multiply(BigDecimal.valueOf(quantidade));
-	}
-
-	private static BigDecimal multiplicaPrecoSessao(Sessao sessao, double multiplicador) {
-		return sessao.getPreco().multiply(BigDecimal.valueOf(multiplicador));
-	}
-
-	private static BigDecimal calculaPrecoSessao(Sessao sessao, double multiplicador) {
-		return sessao.getPreco().add(multiplicaPrecoSessao(sessao, multiplicador));
-	}
-
-	private static boolean verificarQuantidadeIngressos(Sessao sessao, double limite) {
-		return (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= limite;
-	}
->>>>>>> 46d648e68d0b7ae8854a2030129b5db20805dc4c
 }
